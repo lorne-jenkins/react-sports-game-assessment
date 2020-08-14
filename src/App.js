2e49710882241a8723c2570375d5aac57a39135e
Lorne J <biglornej@gmail.com>
2020-08-14 15:53:04 -0600

    get er done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
+
 import React from 'react';
-import logo from './logo.svg';
+import eaglesLogo from './assets/images/eagles2.jpg';
+import chihauhausLogo from './assets/images/angry-chihuahua.jpg';
+import bullyLogo from './assets/images/da-bull.jpg';
+import gcLogo from './assets/images/madbird.jpg';
+import Game from './components/game/Game';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(props) {
+    const eagles = {
+        name: 'Eagles',
+        logoSrc: eaglesLogo
+    }
+    const chihuahuas = {
+        name: 'Angry Chihuahuas',
+        logoSrc: chihauhausLogo
+    }
+    const bully = {
+        name: 'Bull Shite',
+        logoSrc: bullyLogo
+    }
+    
+    const rooster = {
+        name: 'Game Cocks',
+        logoSrc: gcLogo
+    }
+    return (
+        <div className="App">
+            <Game venue="The Garden"
+                homeTeam={chihuahuas}
+                visitingTeam={eagles} />
+            <Game venue="The Dog Pound"
+                homeTeam={bully}
+                visitingTeam={rooster} />
 
+        </div>
+    )
+}
 export default App;
